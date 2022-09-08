16c51b322a9c1881f3a4c7ffa7a55b9bcafa3711
liudongdong <liudongdong@liudongdongdeMacBook-Pro.local>
2022-09-08 15:26:22 +0800

    dev: update 14.最长公共前缀

--- leetcode/01-最长公公前缀.js
+++ leetcode/01-最长公公前缀.js
@@ -1,1 +1,47 @@
-console.log('test')
+/* 
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。
+*/
+const strs = ["flower", "flow", "flight",'fff']
+var longestCommonPrefix = function (strs) {
+    // const len = strs.length
+    // // console.log(len)
+    // const resArr = []
+    // for (let i = 0; i < len; i++) {
+    //     let strLength = strs[i].length
+    //     let con = ''
+    //     let pub = []
+    //     for (let j = 0; j < strLength; j++) {
+    //         // 当前字符
+    //         let str = strs[i].charAt(j)
+    //         con += str
+    //         pub.push(con)
+    //     }
+    //     // console.log(pub)
+    //     resArr.push(pub)
+    // }
+    // console.log(resArr)
+    // let arr = []
+    // for (let i = 0; i < resArr.length; i++) {
+    //     let temp = resArr[i][0]
+    //     arr.push(temp)
+    //     console.log(arr)
+    //     for (let j = 0; j < resArr[i].length; j++) {
+    //         console.log(resArr[i][j])
+    //     }
+    // }
+    let re = ''
+    if(!strs.length)return re
+    // 表示第i位字符
+    for (let i = 0; i < strs[0].length; i++) {
+        // 表示第j个字符串
+        for (let j = 1; j < strs.length; j++) {
+            // 说明没有公共前缀
+            if(strs[j][i]!=strs[0][i]) return re
+        }
+        re += strs[0][i]
+    }
+    return re
+};
+console.log(longestCommonPrefix(strs))
