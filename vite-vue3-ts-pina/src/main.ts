941a8a574a0441299b5a88f3051aaebd5f3a8d49
liudongdong <liudongdong@liudongdongdeMacBook-Pro.local>
2023-02-08 16:57:32 +0800

    feat: 引入 antd  组件库

--- vite-vue3-ts-pina/src/main.ts
+++ vite-vue3-ts-pina/src/main.ts
@@ -3,12 +3,14 @@ import './style.css';
 import App from './App';
 import store from './store';
 import router from '@/router';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue';
 
 // 创建vue 实例
 const app = createApp(App);
 
 // 挂在 pinia
-app.use(store).use(router);
+app.use(store).use(router).use(Antd);
 
 // 挂载实例
 app.mount('#app');
