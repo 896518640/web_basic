822ca560dd0520be18c5f0a3380c94cb922a9640
liudongdong <liudongdong@liudongdongdeMacBook-Pro.local>
2023-02-22 10:49:26 +0800

    feat: compile class

--- vue2-components-dev/kvue/kvue.js
+++ vue2-components-dev/kvue/kvue.js
@@ -11,6 +11,8 @@ function defineReactive(obj,key,val) {
         set(newVal){
             if(newVal!==val) {
                 console.log('set', key)
+                // 防止用户再次设置一个新的对象
+                Observe(newVal)
                 val = newVal
             }
         }
@@ -30,6 +32,19 @@ function set(obj,key,val) {
     defineReactive(obj,key,val)
 }
 
+// 将data中的响应式数据 直接挂载到 vue 实例上
+function proxy(vm) {
+    Object.keys(vm.$data).forEach(key=> {
+        Object.defineProperty(vm,key,{
+            get() {
+                return vm.$data[key]
+            },
+            set(v) {
+                vm.$data[key] = v
+            }
+        })
+    })
+}
 class Vue {
     constructor(options) {
         // 0. 保存选项
@@ -38,6 +53,41 @@ class Vue {
         console.log(this.$data)
         // 1. 响应式  递归遍历data中的对象 做响应式处理3 
         Observe(this.$data)
+
+        // 1.5 proxy
+        proxy(this)
+
         // 2. 编译模板
+        // 需要处理 文本节点判断是否有{{}}   标签节点 v-on v-model @ : 这些属性
+        new Compile(options.el,this)
+    }
+}
+// 遍历模板树 解析动态的部分  并且初始化更新函数
+class Compile{
+    constructor(dom,vm){
+        // 宿主元素
+        let el = document.querySelector(dom)
+
+        // 编译
+        this.compile(el)
+    }
+    isElement(node) {
+        return node.nodeType===1
+    }
+    compile(el) {
+        // 遍历 获得子节点
+        // childNodes 可以获取到 文本节点 和 标签节点
+        const childNodes = el.childNodes
+
+        // children 只能获取到标签节点
+        const childNodes2 = el.children
+        console.log(childNodes,childNodes2)
+        childNodes2.forEach(node=> {
+            if(this.isElement(node)) {
+                // 元素
+            }else if(this.isInter(node)){
+                // 差值绑定表达式
+            }
+        })
     }
 }
